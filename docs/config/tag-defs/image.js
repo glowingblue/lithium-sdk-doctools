ee5b43b583cfc42bc823ced96bdb13901a336535
nikhilmodak <nikhil.modak@lithium.com>
2015-05-26 11:15:10 -0700

    code clean up and re-organization

--- docs/config/tag-defs/image.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    name: 'image',
-    multi: true,
-    transforms: function(doc, tag, value) {
-        var content = value.replace(/^\s+|\s+$/g, '');
-        return content;
-    }
-};
