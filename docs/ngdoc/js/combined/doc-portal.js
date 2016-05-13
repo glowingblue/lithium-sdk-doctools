60ccbc8181ceb709130f55dffb40628e07189cbd
Jake <jake.rozin@gmail.com>
2016-05-13 15:30:53 -0700

    adding doc portal js

---
+++ docs/ngdoc/js/combined/doc-portal.js
@@ -0,0 +1,25 @@
+;(function () {
+  function postUrlMessage() {
+    var messageObj = {
+      event: 'urlChange',
+      data: {}
+    }
+
+    if (window.location.pathname.length > 1) {
+      messageObj.data.p = window.location.pathname;
+    }
+    if (window.location.search.length > 0) {
+      messageObj.data.s = window.location.search;
+    }
+    if (window.location.hash.length > 1) {
+      messageObj.data.h = window.location.hash;
+    }
+
+    window.top.postMessage('l::' + JSON.stringify(messageObj), '*');
+  }
+
+  if (window.JSON) {
+    window.addEventListener('load', postUrlMessage);
+    window.addEventListener('hashchange', postUrlMessage);
+  }
+})();
